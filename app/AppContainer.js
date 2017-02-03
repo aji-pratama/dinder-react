90826dffcbd77c6eadb4c8cf8abf0e66148c9f37
ftumj259 <2013470259@ftumj.ac.id>
2017-02-03 15:54:15 +0700

    Side Menu Done

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Drawer, View } from 'native-base'
 import { Navigator } from 'react-native'
+import SideMenu from './components/sideMenu'
 
 export default class AppContainer extends Component {
   constructor(props) {
@@ -10,48 +11,45 @@ export default class AppContainer extends Component {
       store: {},
       theme: null
     }
+  }
 
-    toggleDrawer(){
-      this.state.toggled ? this._drawer.close() : this._drawer.open()
-    }
-
-    openDrawer(){
-      this.setState({toggled: true})
-    }
-
-    closeDrawer(){
-      this.setState({toggled: false})
-    }
-
-    renderScene(route, navigator){
-      switch(route) {
-        default: {
-          return null
-        }
+  toggleDrawer(){
+    this.state.toggled ? this._drawer.close() : this._drawer.open()
+  }
+  openDrawer(){
+    this.setState({toggled: true})
+  }
+  closeDrawer(){
+    this.setState({toggled: false})
+  }
+  renderScene(route, navigator){
+    switch(route) {
+      default: {
+        return null
       }
     }
-
-    configureScene(route, routeStack) {
-      return Navigator.ScenceConfig.PushFromRight
-    }
-
-    render() {
-      return (
-        <Drawer
-          ref={(ref) => this._drawer = ref}
-          type="displace"
-          content={<View style={{backgroundColor: "#000", height: 1000}}/>}
-          onClose={this.closeDrawer.bind(this)}
-          onOpen={this.openDrawer.bind(this)}
-          openDrawerOffset={0.2}
-          >
-            <Navigator
-              ref={(ref) => this._navigator = ref}
-              configureScene={this.configureScene.bind(this)}
-              renderScene={this.renderScene.bind(this)}
-              />
-          </Drawer>
-        )
-    }
   }
+  configureScene(route, routeStack) {
+    return Navigator.SceneConfigs.PushFromRight
+  }
+
+  render() {
+    return (
+      <Drawer
+        ref={(ref) => this._drawer = ref}
+        type="displace"
+        content={<SideMenu navigator={this._navigator} theme={this.state.theme} />}
+        onClose={this.closeDrawer.bind(this)}
+        onOpen={this.openDrawer.bind(this)}
+        openDrawerOffset={0.2}
+        >
+          <Navigator
+            ref={(ref) => this._navigator = ref}
+            configureScene={this.configureScene.bind(this)}
+            renderScene={this.renderScene.bind(this)}
+            />
+        </Drawer>
+      )
+  }
+
 }

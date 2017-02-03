dacdcab19926a12a8027376c41d67cdc1d8231bb
ftumj259 <2013470259@ftumj.ac.id>
2017-02-03 17:05:23 +0700

    SplashScene Done Step 2 ofcourse Done, andNext step 3

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -8,13 +8,17 @@ import SplashScene from './scenes/splashScene'
 
 import theme from './theme/base-theme'
 
+const settings = new SettingStore()
+
 export default class AppContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
       toggled: false,
-      store: {},
-      theme: null
+      store: {
+        settings: settings
+      },
+      theme: theme
     }
   }
 
@@ -28,7 +32,10 @@ export default class AppContainer extends Component {
     this.setState({toggled: false})
   }
   renderScene(route, navigator){
-    switch(route) {
+    switch(route.title) {
+      case 'Splash': {
+        return <SplashScene {...route.passProps} navigator={navigator} />
+      }
       default: {
         return null
       }
@@ -52,6 +59,14 @@ export default class AppContainer extends Component {
             ref={(ref) => this._navigator = ref}
             configureScene={this.configureScene.bind(this)}
             renderScene={this.renderScene.bind(this)}
+            initialRoute={{
+              title: "Splash",
+              passProps: {
+                stores: this.state.store,
+                toggleDrawer: this.toggleDrawer.bind(this),
+                theme: this.state.theme
+              }
+            }}
             />
         </Drawer>
       )
